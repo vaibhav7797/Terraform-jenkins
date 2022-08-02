f1dafc8c0a13d055d6f9e0e774debca4ccf7db0d
Vaibhav Bhagwat Pendhare <pendharevaibhav@gmail.com>
2022-08-02 19:38:56 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source  = "clouddrove/vpc/aws"
+  version = "0.15.1"
+
+  name        = "vpc"
+  environment = "test"
+  label_order = ["name", "environment"]
+
+  cidr_block = "172.16.0.0/16"
+}
